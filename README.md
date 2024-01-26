767da84f7bdc13de8491abc64a8c3a7aa1a03ba0
diegofranciscodasilva <diegofranciscodasilva@yahoo.com.br>
2024-01-26 07:38:57 -0300

    projeto jogo adivinhe a palavra

---
+++ README.md
@@ -0,0 +1,45 @@
+# Jogo Adivinhe a Palavra
+
+Bem-vindo ao Jogo de Adivinhação de Palavras ! Este projeto foi desenvolvido com HTML, CSS e JavaScript para criar um jogo interativo onde o jogador tenta adivinhar uma palavra proposta.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Funcionalidades
+
+- **Adivinhação de Palavras**: Tente adivinhar a palavra proposta preenchendo as letras corretas.
+- **Controle de Erros**: A cada letra errada, é mostrado na tela.
+- **Feedback Interativo**: Receba feedback instantâneo sobre suas escolhas.
+- **Botão de Reinício**: Reinicie o jogo a qualquer momento para jogar novamente.
+- **Dica da Palavra**: Obtenha uma dica sobre a palavra proposta.
+- **Números de Chances**: A quantidade de chances restantes é mostrada na tela.
+- **Design Responsivo e Atraente**: Interface fácil de usar em qualquer dispositivo.
+
+## Como Jogar
+
+Baixe ou clone o repositório em seu computador.
+Abra o arquivo `index.html` em seu navegador web.
+Comece a adivinhar as palavras e divirta-se!
+
+## Estrutura do Projeto
+
+- `index.html`: Contém a estrutura HTML da página principal com o jogo.
+- `style.css`: Arquivo de estilo CSS para estilizar o jogo.
+- `script.js`: Script JavaScript para adicionar a lógica do jogo.
+
+## Personalização
+
+- Modifique o arquivo `index.html` para ajustar a estrutura conforme necessário.
+- Adapte o estilo no arquivo `style.css` para atender às suas preferências.
+- Explore o arquivo `script.js` para ajustar a lógica do jogo ou adicionar mais funcionalidades, se necessário.
+
+## Pré-requisitos
+
+Certifique-se de ter um navegador web instalado para visualizar o projeto.
+
+## Como Contribuir
+
+Sinta-se à vontade para contribuir abrindo problemas (issues) ou enviando pull requests.
---
+++ assets/js/script.js
@@ -0,0 +1,419 @@
+let wordList = [
+    {
+        word: 'banana',
+        hint: 'fruta amarela'
+    },
+    {
+        word: 'doce',
+        hint: 'guloseima'
+    },
+    {
+        word: 'github',
+        hint: 'plataforma de códigos'
+    },
+    {
+        word: 'png',
+        hint: 'formato de imagem'
+    },
+    {
+        word: 'prata',
+        hint: 'tipo de metal'
+    },
+    {
+        word: 'celular',
+        hint: 'telefone móvel'
+    },
+    {
+        word: 'gpu',
+        hint: 'componente de PC'
+    },
+    {
+        word: 'java',
+        hint: 'linguagem de programação'
+    },
+    {
+        word: 'google',
+        hint: 'página de procura'
+    },
+    {
+        word: 'veneza',
+        hint: 'famosa cidade das águas'
+    },
+    {
+        word: 'excel',
+        hint: 'programa da microsoft'
+    },
+    {
+        word: 'dança',
+        hint: 'movimento na música'
+    },
+    {
+        word: 'aguia',
+        hint: 'caçador aéreo'
+    },
+    {
+        word: 'uva',
+        hint: 'fruta pequena'
+    },
+    {
+        word: 'mel',
+        hint: 'abelhas fazem'
+    },
+    {
+        word: 'geleia',
+        hint: 'doce mole'
+    },
+    {
+        word: 'limao',
+        hint: 'fruta ácida'
+    },
+    {
+        word: 'melao',
+        hint: 'fruta amarela'
+    },
+    {
+        word: 'azeitona',
+        hint: 'faz óleo'
+    },
+    {
+        word: 'pessego',
+        hint: 'têm calda'
+    },
+    {
+        word: 'rainha',
+        hint: 'esposa do rei'
+    },
+    {
+        word: 'coelho',
+        hint: 'animal famoso'
+    },
+    {
+        word: 'aranha',
+        hint: 'têm filmes dele'
+    },
+    {
+        word: 'tigre',
+        hint: 'gato grande'
+    },
+    {
+        word: 'tio',
+        hint: 'irmão do pai'
+    },
+    {
+        word: 'virus',
+        hint: 'doença'
+    },
+    {
+        word: 'agua',
+        hint: 'solvente universal'
+    },
+    {
+        word: 'xenonio',
+        hint: 'elemento químico'
+    },
+    {
+        word: 'iate',
+        hint: 'barco chic'
+    },
+    {
+        word: 'pao',
+        hint: 'têm francês'
+    },
+    {
+        word: 'queijo',
+        hint: 'made in minas'
+    },
+    {
+        word: 'burro',
+        hint: 'parente do cavalo'
+    },
+    {
+        word: 'omelete',
+        hint: 'misturada de ovo'
+    },
+    {
+        word: 'geladeira',
+        hint: 'eletrodoméstico'
+    },
+    {
+        word: 'guitarra',
+        hint: 'violão elétrico'
+    },
+    {
+        word: 'hockey',
+        hint: 'esporte gelado'
+    },
+    {
+        word: 'iglu',
+        hint: 'casa de gelo'
+    },
+    {
+        word: 'jaqueta',
+        hint: 'blusa chic'
+    },
+    {
+        word: 'chaleira',
+        hint: 'esquenta água'
+    },
+    {
+        word: 'folhas',
+        hint: 'partes da árvores'
+    },
+    {
+        word: 'louvaadeus',
+        hint: 'inseto bravo'
+    },
+    {
+        word: 'macarrao',
+        hint: 'comida italiana'
+    },
+    {
+        word: 'laranja',
+        hint: 'fruta cítrica'
+    },
+    {
+        word: 'pimenta',
+        hint: 'ardida'
+    },
+    {
+        word: 'mysql',
+        hint: 'sistema de banco de dados'
+    },
+    {
+        word: 'brasil',
+        hint: 'país tropical'
+    },
+    {
+        word: 'flauta',
+        hint: 'instrumento de sopro'
+    },
+    {
+        word: 'crypto',
+        hint: 'moeda digital'
+    },
+    {
+        word: 'tesla',
+        hint: 'famoso cientista'
+    },
+    {
+        word: 'marte',
+        hint: 'planeta vermelho'
+    },
+    {
+        word: 'proxy',
+        hint: 'aplicação de servidor'
+    },
+    {
+        word: 'email',
+        hint: 'mensagem eletrônica'
+    },
+    {
+        word: 'html',
+        hint: 'linguagem de programação'
+    },
+    {
+        word: 'ar',
+        hint: 'respiração'
+    },
+    {
+        word: 'ideia',
+        hint: 'pensamento'
+    },
+    {
+        word: 'servidor',
+        hint: 'sistema para PC'
+    },
+    {
+        word: 'svg',
+        hint: 'formato de imagem'
+    },
+    {
+        word: 'jpeg',
+        hint: 'formato de imagem'
+    },
+    {
+        word: 'procurar',
+        hint: 'buscar alguma coisa'
+    },
+    {
+        word: 'chave',
+        hint: 'abre portas'
+    },
+    {
+        word: 'egito',
+        hint: 'país dos faraós'
+    },
+    {
+        word: 'coringa',
+        hint: 'personagem famoso'
+    },
+    {
+        word: 'dubai',
+        hint: 'país do oriente médio'
+    },
+    {
+        word: 'foto',
+        hint: 'recordação visual'
+    },
+    {
+        word: 'nilo',
+        hint: 'mais largo rio do mundo'
+    },
+    {
+        word: 'chuva',
+        hint: 'cai do céu'
+    },
+    {
+        word: 'python',
+        hint: 'linguagem de programação'
+    },
+    {
+        word: 'violao',
+        hint: 'instrumento musical'
+    },
+    {
+        word: 'mirar',
+        hint: 'alvo'
+    },
+    {
+        word: 'venus',
+        hint: 'planeta'
+    },
+    {
+        word: 'ouro',
+        hint: 'metal precioso'
+    },
+    {
+        word: 'shopee',
+        hint: 'shoping on-line'
+    },
+    {
+        word: 'golang',
+        hint: 'linguagem de programação'
+    },
+    {
+        word: 'codigo',
+        hint: 'relativo a programação'
+    },
+    {
+        word: 'matrix',
+        hint: 'famoso filme de ficção'
+    },
+    {
+        word: 'bugs',
+        hint: 'relacioando a programação'
+    },
+    {
+        word: 'avatar',
+        hint: 'filme épico com cgi'
+    },
+    {
+        word: 'gif',
+        hint: 'formato de imagem'
+    },
+    {
+        word: 'mental',
+        hint: 'relativo a mente'
+    },
+    {
+        word: 'mapa',
+        hint: 'diagrama de área'
+    },
+    {
+        word: 'ilha',
+        hint: 'terra com água em volta'
+    },
+    {
+        word: 'futebol',
+        hint: 'esporte famoso'
+    },
+    {
+        word: 'xadrez',
+        hint: 'jogo de tabuleiro'
+    },
+    {
+        word: 'instagram',
+        hint: 'rede social famosa'
+    },
+]
+
+const inputs = document.querySelector('.word')
+const typeInput = document.querySelector('.type-input')
+const guess = document.querySelector('.guess span')
+const mistakes = document.querySelector('.wrong span')
+const hintTag = document.querySelector('.hint span')
+const hintElement = document.querySelector('.hint')
+const resetBtn = document.querySelector('.reset')
+const hintBtn = document.querySelector('.showhint')
+
+let word
+let incorrectLetters = []
+let correctLetters = []
+let maxGuesses
+
+function startNewGame() {
+    alert('Começa o jogo ! Adivinhe a palavra :)')
+    hintElement.style.display = 'none'
+    hintElement.style.opacity = 0
+
+    const ranWord = wordList[Math.floor(Math.random() * wordList.length)]
+    word = ranWord.word
+
+    maxGuesses = word.length >= 5 ? 8 : 6
+    incorrectLetters = []
+    correctLetters = []
+    hintTag.innerText = ranWord.hint
+    guess.innerText = maxGuesses
+    mistakes.innerText = incorrectLetters
+
+    inputs.innerHTML = ''
+    for(let i = 0; i< word.length; i++) {
+        const input = document.createElement('input')
+        input.type = 'text'
+        input.disabled = true
+        inputs.appendChild(input)
+    }
+}
+
+function handleInput(e) {
+    const key = e.target.value.toLowerCase()
+    if(key.match(/^[a-z]+$/i) && !incorrectLetters.includes(`${key}`) && !correctLetters.includes(`${key}`)) {
+        if(word.includes(key)) {
+            for(let i = 0; i < word.length; i++) {
+                if(word[i] === key) {
+                    inputs.querySelectorAll('input')[i].value += key
+                }
+            }
+            correctLetters += key
+        } else {
+            maxGuesses--
+            incorrectLetters.push(`${key}`)
+            mistakes.innerText = incorrectLetters
+        }
+    }
+    guess.innerText = maxGuesses
+
+    if(correctLetters.length === word.length) {
+        alert(`Parabéns ! Você encontrou a palavra ${word.toUpperCase()}`)
+        startNewGame()
+    } else if(maxGuesses < 1) {
+        alert('Game Over ! Acabou suas chances :(')
+        for(let i = 0; i < word.length; i++) {
+            inputs.querySelectorAll('input')[i].value = word[i]
+        }
+    }
+    typeInput.value = ''
+}
+
+function showHintElement() {
+    hintElement.style.display = 'block'
+    hintElement.style.opacity = '1'
+}
+
+resetBtn.addEventListener('click', startNewGame)
+hintBtn.addEventListener('click', showHintElement)
+typeInput.addEventListener('input', handleInput)
+inputs.addEventListener('click', () => typeInput.focus())
+document.addEventListener('keydown', () => typeInput.focus())
+
+startNewGame()
